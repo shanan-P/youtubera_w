f2ca2cf05bcb9cd5a76681ab38db8e56cb140152
Ansh Pancholi <pancholiansh17@gmail.com>
2025-10-13 16:24:41 +0530

    Update build script

--- web/render-build.sh
+++ web/render-build.sh
@@ -5,12 +5,12 @@ set -o errexit
 # 1. Install project dependencies
 npm install
 
-# 2. Create a directory for our binaries
-mkdir -p ./.bin
+# 2. Create a directory for our binaries inside the web directory
+mkdir -p web/.bin
 
 # 3. Download and install yt-dlp
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o ./.bin/yt-dlp
-chmod a+rx ./.bin/yt-dlp
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o web/.bin/yt-dlp
+chmod a+rx web/.bin/yt-dlp
 
 # 4. Install ffmpeg
 #    Render's native environments are Debian-based, so we can use apt-get.
